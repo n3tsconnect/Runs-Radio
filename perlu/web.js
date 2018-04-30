7de9af3a6abd696ab6b25593af62082753d50a23
Rafi Priatna K <stormrider136@GMAIL.COM>
2018-04-30 22:47:25 +0700

    Adding messages when radio is offline

--- perlu/web.js
+++ perlu/web.js
@@ -12,7 +12,10 @@ document.onkeydown = function(e) {
     }
 };
 
-// Radio
+// Radio Cek online / offline
     document.getElementById('radio').addEventListener("error",function() {
         alert('Runs Radio is OFFLINE');
+    document.getElementById('audioplayer').style ="Display: none;";
+    document.getElementById("oflen").style = "color:red";
+    document.getElementById("oflen").innerHTML = "Sorry, Runs Radio is offline :(";
     },false);
