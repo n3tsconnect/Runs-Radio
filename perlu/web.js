d863c57fd0f0f870df0af0232777556ceaa82932
Rafi Priatna K <stormrider136@GMAIL.COM>
2018-05-01 11:20:28 +0700

    Change color

--- perlu/web.js
+++ perlu/web.js
@@ -13,9 +13,9 @@ document.onkeydown = function(e) {
 };
 
 // Radio Cek online / offline
-    document.getElementById('radio').addEventListener("error",function() {
-        alert('Runs Radio is OFFLINE');
-    document.getElementById('audioplayer').style ="Display: none;";
-    document.getElementById("oflen").style = "color:red";
-    document.getElementById("oflen").innerHTML = "Sorry, Runs Radio is offline :(";
-    },false);
+document.getElementById('radio').addEventListener("error",function() {
+    alert('Runs Radio is OFFLINE');
+document.getElementById('audioplayer').style ="Display: none;";
+document.getElementById("oflen").style = "color:#e74c3c";
+document.getElementById("oflen").innerHTML = "Sorry, Runs Radio is offline :(";
+},false);
